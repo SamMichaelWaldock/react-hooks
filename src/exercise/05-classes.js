a333c1dd46b1a565b7276529e146a9c15f5ac5f3
Sam Waldock <swa@nexiosit.com>
2023-06-21 14:15:08 +0200

    useRef and useEffect (DOM interaction): Class Refactor

--- src/exercise/05-classes.js
+++ src/exercise/05-classes.js
@@ -5,38 +5,34 @@
 import * as React from 'react'
 import VanillaTilt from 'vanilla-tilt'
 
-// If you'd rather practice refactoring a class component to a function
-// component with hooks, then go ahead and do this exercise.
+const Tilt = ({ children }) => {
+  const tiltRef = React.useRef();
 
-class Tilt extends React.Component {
-  tiltRef = React.createRef()
-  componentDidMount() {
-    const tiltNode = this.tiltRef.current
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current;
+
     const vanillaTiltOptions = {
       max: 25,
       speed: 400,
       glare: true,
       'max-glare': 0.5,
-    }
-    VanillaTilt.init(tiltNode, vanillaTiltOptions)
-  }
-  componentWillUnmount() {
-    this.tiltRef.current.vanillaTilt.destroy()
-  }
-  render() {
-    return (
-      <div ref={this.tiltRef} className="tilt-root">
-        <div className="tilt-child">{this.props.children}</div>
-      </div>
-    )
-  }
-}
-function App() {
+    };
+    VanillaTilt.init(tiltNode, vanillaTiltOptions);
+
+    return () => tiltNode.vanillaTilt.destroy();
+  }, [])
+
   return (
-    <Tilt>
-      <div className="totally-centered">vanilla-tilt.js</div>
-    </Tilt>
+    <div ref={tiltRef} className="tilt-root">
+      <div className="tilt-child">{children}</div>
+    </div>
   )
 }
+
+const App = () => (
+  <Tilt>
+    <div className="totally-centered">vanilla-tilt.js</div>
+  </Tilt>
+)
 
 export default App
